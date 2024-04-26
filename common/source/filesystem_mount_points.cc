6f6c62a07c01c401814208f500f7e5b37fc9d08a
Mateusz Stadnik <matgla@live.com>
2024-04-26 21:58:43 +0200

    porting flags

--- common/source/filesystem_mount_points.cc
+++ common/source/filesystem_mount_points.cc
@@ -27,18 +27,20 @@ module;
 
 export module yasboot.filesystem.filesystem_mount_points;
 
-import yasboot.filesystem;
+export import yasboot.filesystem;
 
-export namespace yasboot::fs {
-class FileSystemMountPoints {
+export namespace yasboot::fs
+{
+class FileSystemMountPoints
+{
 public:
   using MountPoints = std::map<std::string, std::unique_ptr<FileSystem>>;
 
   int register_mount_point(std::string_view path,
                            std::unique_ptr<FileSystem> filesystem);
 
-  std::pair<FileSystem *, std::string_view>
-  get_mount_point(std::string_view pathname);
+  std::pair<FileSystem *, std::string_view> get_mount_point(
+    std::string_view pathname);
 
   const FileSystem *get_filesystem_for_fd(int fd) const;
 
@@ -51,11 +53,14 @@ private:
 
 } // namespace yasboot::fs
 
-namespace yasboot::fs {
+namespace yasboot::fs
+{
 
 int FileSystemMountPoints::register_mount_point(
-    std::string_view path, std::unique_ptr<FileSystem> filesystem) {
-  if (mount_points_.count(std::string(path))) {
+  std::string_view path, std::unique_ptr<FileSystem> filesystem)
+{
+  if (mount_points_.count(std::string(path)))
+  {
     return -1;
   }
 
@@ -63,16 +68,20 @@ int FileSystemMountPoints::register_mount_point(
   return 0;
 }
 
-FileSystemMountPoints &FileSystemMountPoints::get() {
+FileSystemMountPoints &FileSystemMountPoints::get()
+{
   static FileSystemMountPoints instance;
   return instance;
 }
 
-std::pair<FileSystem *, std::string_view>
-FileSystemMountPoints::get_mount_point(std::string_view pathname) {
+std::pair<FileSystem *, std::string_view> FileSystemMountPoints::get_mount_point(
+  std::string_view pathname)
+{
   std::pair<FileSystem *, std::string_view> result{};
-  for (const auto &[path, filesystem] : mount_points_) {
-    if (pathname.starts_with(path) && path.size() > result.second.size()) {
+  for (const auto &[path, filesystem] : mount_points_)
+  {
+    if (pathname.starts_with(path) && path.size() > result.second.size())
+    {
       result = std::make_pair(filesystem.get(), pathname.substr(path.size()));
     }
   }
@@ -80,9 +89,12 @@ FileSystemMountPoints::get_mount_point(std::string_view pathname) {
   return result;
 }
 
-const FileSystem *FileSystemMountPoints::get_filesystem_for_fd(int fd) const {
-  for (const auto &[path, fs] : mount_points_) {
-    if (fs->has_fd(fd)) {
+const FileSystem *FileSystemMountPoints::get_filesystem_for_fd(int fd) const
+{
+  for (const auto &[path, fs] : mount_points_)
+  {
+    if (fs->has_fd(fd))
+    {
       return fs.get();
     }
   }
--- cpu/arm/v6-m/common/source/system_stubs.cpp
+++ cpu/arm/v6-m/common/source/system_stubs.cpp
@@ -24,99 +24,126 @@
 
 import hal.system.io;
 
-extern "C" {
-void panic(const char *, ...) {
-  while (true) {
-  }
-}
+import yasboot.filesystem.filesystem_mount_points;
 
-void __unhandled_user_irq() {
-  while (true) {
-  }
-}
-
-void hard_assertion_failure() {
-  while (true) {
-  }
-}
-
-int __attribute__((used)) _kill(pid_t, int) { return 0; }
-
-pid_t __attribute__((used)) _getpid(void) { return 0; }
-
-int __attribute__((used)) _fstat(int, struct stat *) { return 0; }
-
-int __attribute((used)) _isatty(int) { return 0; }
-
-void __attribute__((used)) _exit(int) {
-  while (true) {
-  }
-}
-
-int __attribute__((used)) _close(int fd) {
-  // auto fs =
-  // yasboot::fs::FilesystemMountPoints::get().get_filesystem_for_fd(fd); if (fs
-  // == nullptr) {
-  //   return -1;
-  // }
-  // return fs->close(fd);
-  static_cast<void>(fd);
-  return -1;
-}
-
-off_t __attribute__((used)) _lseek(int, off_t, int) { return 0; }
-
-ssize_t __attribute__((used)) _read(int fd, void *buf, size_t size) {
-  // auto fs =
-  // yasboot::fs::FilesystemMountPoints::get().get_filesystem_for_fd(fd); if (fs
-  // == nullptr) {
-  //   return 0;
-  // }
-  // return fs->read_file(fd,
-  //                      std::span<uint8_t>(static_cast<uint8_t *>(buf),
-  //                      size));
-  static_cast<void>(fd);
-  static_cast<void>(buf);
-  static_cast<void>(size);
-  return 0;
-}
-
-ssize_t __attribute__((used)) _write(int fd, const char *buf, size_t count) {
-  const auto write = yasboot::hal::system::io::get_global_write();
-  if ((fd == 1 || fd == 2) && write) {
-    return write(std::string_view(buf, count));
-  }
-  return 0;
-}
-
-extern char __heap_start__;
-extern char __heap_end__;
-
-static char *current_heap_end = &__heap_start__;
-
-void *_sbrk(intptr_t incr) {
-  if (current_heap_end + incr > &__heap_end__) {
-    return nullptr;
+extern "C"
+{
+  void panic(const char *, ...)
+  {
+    while (true)
+    {
+    }
   }
 
-  char *previous_heap_end = current_heap_end;
-  current_heap_end += incr;
-  return static_cast<caddr_t>(previous_heap_end);
-}
+  void __unhandled_user_irq()
+  {
+    while (true)
+    {
+    }
+  }
 
-int __attribute__((used)) _open(const char *pathname, int flags) {
-  // auto [fs, path] =
-  //     yasboot::fs::FilesystemMountPoints::get().get_mount_point(pathname);
+  void hard_assertion_failure()
+  {
+    while (true)
+    {
+    }
+  }
 
-  // if (!fs) {
-  //   printf("Responding with -1\n");
-  //   errno = ENOENT;
-  //   return -1;
-  // }
+  int __attribute__((used)) _kill(pid_t, int)
+  {
+    return 0;
+  }
 
-  // return fs->open(path, flags);system_st
-  static_cast<void>(pathname);
-  static_cast<void>(flags);
-  return -1;
-}
+  pid_t __attribute__((used)) _getpid(void)
+  {
+    return 0;
+  }
+
+  int __attribute__((used)) _fstat(int, struct stat *)
+  {
+    return 0;
+  }
+
+  int __attribute((used)) _isatty(int)
+  {
+    return 0;
+  }
+
+  void __attribute__((used)) _exit(int)
+  {
+    while (true)
+    {
+    }
+  }
+
+  int __attribute__((used)) _close(int fd)
+  {
+    auto fs = yasboot::fs::FileSystemMountPoints::get().get_filesystem_for_fd(fd);
+    if (fs == nullptr)
+    {
+      return -1;
+    }
+    return fs->close(fd);
+  }
+
+  off_t __attribute__((used)) _lseek(int, off_t, int)
+  {
+    return 0;
+  }
+
+  ssize_t __attribute__((used)) _read(int fd, void *buf, size_t size)
+  {
+    auto fs = yasboot::fs::FileSystemMountPoints::get().get_filesystem_for_fd(fd);
+    if (fs == nullptr)
+    {
+      return 0;
+    }
+    return fs->read_file(fd, std::span<uint8_t>(static_cast<uint8_t *>(buf), size));
+  }
+
+  ssize_t __attribute__((used)) _write(int fd, const char *buf, size_t count)
+  {
+    const auto write = yasboot::hal::system::io::get_global_write();
+    if ((fd == 1 || fd == 2) && write)
+    {
+      return write(std::string_view(buf, count));
+    }
+    return 0;
+  }
+
+  extern char __heap_start__;
+  extern char __heap_end__;
+
+  static char *current_heap_end = &__heap_start__;
+
+  void *_sbrk(intptr_t incr)
+  {
+    if (current_heap_end + incr > &__heap_end__)
+    {
+      return nullptr;
+    }
+
+    char *previous_heap_end = current_heap_end;
+    current_heap_end += incr;
+    return static_cast<caddr_t>(previous_heap_end);
+  }
+
+  int __attribute__((used)) _open(const char *pathname, int flags)
+  {
+    auto [fs, path] =
+      yasboot::fs::FileSystemMountPoints::get().get_mount_point(pathname);
+
+    if (!fs)
+    {
+      errno = ENOENT;
+      return -1;
+    }
+
+    const auto r = fs->open(path, flags);
+    if (r)
+    {
+      return *r;
+    }
+    return r.error();
+  }
 }
